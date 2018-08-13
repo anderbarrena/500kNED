db62cfce225f791f2e992fd2479dfadc65d3919f
anderbarrena <38695413+anderbarrena@users.noreply.github.com>
2018-08-13 16:42:14 +0200

    Update 00-do.gather.sh

--- 00-do.gather.sh
+++ 00-do.gather.sh
@@ -13,7 +13,7 @@ perl m@th.[deep].pl -w2v "$vector" -dict source/dict/2014wiki_plato_subset.dict.
 # for LSTM Single Model
 # this may take some hours...
 vector="source/vectors/wikipedia+.d300.w2v.bz2"
-perl m@thNster.[deep].pl -w2v "$vector" -dict source/dict/2014wiki_10occ.dict.bz2 -cluster 2>> debug/gather.m@thNster.txt
+perl m@thNster.[deep].pl -w2v "$vector" -dict source/dict/2014wiki_50occ.dict.bz2 -cluster 2>> debug/gather.m@thNster.txt
 
 # for 500K dataset uncomment and run this for sparse bag of words files:
 # vector="source/clusters/wikipedia+.c3000.cls.bz2"
